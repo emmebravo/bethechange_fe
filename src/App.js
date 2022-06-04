07d5f7718cf7bdc43aff2525e2171ad86278f43f
mbcodes <mbcodes@protonmail.com>
2022-06-03 19:59:49 -0700

    added yarn, modified App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return <div className='App'></div>;
-}
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import Dashboard from './components/Dashboard';
+import NotFound from './components/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+          {/* <PrivateRoute exact path='dashboard' element={<Dashboard />} /> */}
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
